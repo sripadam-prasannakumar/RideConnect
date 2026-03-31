05f306b5936e7e21ef82896b8aa05f8b2daa7a7c
Prasannakumar <sripadamprasannakumar@gmail.com>
2026-03-31 13:46:22 +0530

    Configure default API URL to point to Render Production

--- frontend_project/rideconnect/src/apiConfig.js
+++ frontend_project/rideconnect/src/apiConfig.js
@@ -1,2 +1,2 @@
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://10.1.40.239:8000';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://rideconnect-api-dwd8.onrender.com';
 export default API_BASE_URL;
